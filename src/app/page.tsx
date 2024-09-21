0054c9ec7cb1ccf01bbc6a6630697513427762c4
Anthony <oips.anthony@gmail.com>
2024-09-21 23:24:36 +0800

    refactor(page): add overflow-hidden to main and container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Chat from "../components/chat";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="flex min-h-screen flex-col items-center justify-center overflow-hidden">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 overflow-hidden">
           <Chat />
         </div>
       </main>
