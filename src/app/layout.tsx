e62c043071e5755a6e57edec4ddde10385f936fc
Anthony <oips.anthony@gmail.com>
2024-09-21 23:34:40 +0800

    refactor(layout): add viewport metadata and fix import spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,25 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { type Viewport, type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { NextUIProvider } from "@nextui-org/react";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeSwitch } from "~/components/theme-switch";
 
 export const metadata: Metadata = {
   title: "汉语新解",
   description: "汉语新解",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
