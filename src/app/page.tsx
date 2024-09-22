6e94ed0a26df18ac3485cadc03b9398836c87656
Anthony <oips.anthony@gmail.com>
2024-09-23 00:11:25 +0800

    style(app): adjust div positioning and add whitespace to gradient class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center overflow-hidden">
-        <div className="fixed top-4 left-1/2 transform -translate-x-1/2">
+        <div className="fixed top-8 left-1/2 transform -translate-x-1/2">
           <Aff />
         </div>
         <div className="container flex flex-col items-center justify-center gap-4 px-4 py-16 overflow-hidden">
--- src/components/aff.tsx
+++ src/components/aff.tsx
@@ -11,7 +11,7 @@ export async function Aff() {
         🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
         <span
           className={cn(
-            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent whitespace-nowrap`,
           )}
         >
           2000万Tokens送不停！
