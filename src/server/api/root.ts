2a2fc967e13ecf93a3098d1e9523f4c571c1e764
Anthony <oips.anthony@gmail.com>
2024-09-22 23:35:19 +0800

    feat(api): add example router and hello procedure

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { exampleRouter } from "./routers/example";
 
 /**
  * This is the primary router for your server.
@@ -6,6 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  example: exampleRouter
 });
 
 // export type definition of API
